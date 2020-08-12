ed567dec7382182a6adce814d36d59abcba4faf7
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-12 15:56:09 -0500

    update about section with img

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
-import data from "../yourdata";
+import pic from "../components/pic.png";
 
 const About = () => {
 	return (
@@ -12,12 +12,31 @@ const About = () => {
 					</Fade>
 				</h1>
 				<Fade bottom>
-					<p>{data.abouttext}</p>
+					<p>
+						My name is Yadira Tamarit. I’m originally from Cuba. I’m a mom of
+						two wonderful boys.I have a Bachelor Degree in Computer Science. I
+						am currently enrolled in a UT Coding Bootcamp(The University of
+						Texas at Austin) at Full Stack Developer course in order to learn
+						all new technologies and continue growing my career.
+					</p>
+					<p>
+						I love mathematics for that reason I choose computer science as a
+						career. Professionally, I have always liked to challenge myself by
+						never giving up on any difficult task. I also consider myself a team
+						player, able to collaborate and help others. I'm dedicated,
+						hard-worker, highly organized and able to work under pressure.
+					</p>
+					<p>
+						I love studying and I’m open to learn when is need it. I studied
+						Computer Sciences in (Universidad de las Ciencias Informaticas)
+						located in Havana, Cuba from (2003 - 2008).For more information
+						about my professional career, please reference the provided links
+						below:
+					</p>
 				</Fade>
 			</div>
-			{data.ShowAboutImage ? (
-				<img src={data.aboutImage} alt="about iamge"></img>
-			) : null}
+
+			<img src={pic} alt="me" className="rounded" />
 		</div>
 	);
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
 const Navbar = () => {
 	const scrollToTop = () => {
