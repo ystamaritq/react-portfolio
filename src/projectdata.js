b3daaf61b144bfd0b05a995a7eced3b1dcf7612b
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-13 11:49:55 -0500

    added last project and img

--- src/projectdata.js
+++ src/projectdata.js
@@ -7,42 +7,43 @@ import proj6image from "./assets/imgs/projects/project6.png";
 import proj7image from "./assets/imgs/projects/project7.png";
 import proj8image from "./assets/imgs/projects/project8.png";
 import proj9image from "./assets/imgs/projects/project9.png";
+import proj10image from "./assets/imgs/projects/project10.png";
 
 export default [
 	{
 		id: 1,
-		title: "Password Generator",
+		title: "Wedding Planner",
 		service: "GitHub Repo",
-		imageSrc: proj1image,
-		url: "https://github.com/ystamaritq/password-generator",
+		imageSrc: proj10image,
+		url: "https://github.com/wedding-planner-app/wedding-planner",
 	},
 	{
 		id: 2,
-		title: "Work Day Scheduler",
-		service: "GitHub Repo",
-		imageSrc: proj2image,
-		url: "https://github.com/ystamaritq/work-day-scheduler",
-	},
-	{
-		id: 3,
-		title: "Code Quiz",
-		service: "GitHub Repo",
-		imageSrc: proj3image,
-		url: "https://github.com/ystamaritq/code-quiz",
-	},
-	{
-		id: 4,
-		title: "Weather Dashboard",
-		service: "GitHub Repo",
-		imageSrc: proj4image,
-		url: "https://ystamaritq.github.io/weather-dashboard/",
-	},
-	{
-		id: 5,
 		title: "Travel Board",
 		service: "GitHub Repo",
 		imageSrc: proj5image,
 		url: "https://github.com/CB-Group-6-Project-1/travel-board",
+	},
+	{
+		id: 3,
+		title: "Note Taker",
+		service: "GitHub Repo",
+		imageSrc: proj8image,
+		url: "https://github.com/ystamaritq/note-taker",
+	},
+	{
+		id: 4,
+		title: "Team Generator",
+		service: "GitHub Repo",
+		imageSrc: proj7image,
+		url: "https://github.com/ystamaritq/team-profile-generator",
+	},
+	{
+		id: 5,
+		title: "Employee Tracker",
+		service: "GitHub Repo",
+		imageSrc: proj9image,
+		url: "https://github.com/ystamaritq/employee-tracker",
 	},
 	{
 		id: 6,
@@ -51,39 +52,33 @@ export default [
 		imageSrc: proj6image,
 		url: "https://github.com/ystamaritq/readme-generator",
 	},
+
 	{
 		id: 7,
-		title: "Team Generator",
+		title: "Code Quiz",
 		service: "GitHub Repo",
-		imageSrc: proj7image,
-		url: "https://github.com/ystamaritq/team-profile-generator",
+		imageSrc: proj3image,
+		url: "https://github.com/ystamaritq/code-quiz",
 	},
 	{
 		id: 8,
-		title: "Note Taker",
+		title: "Weather Dashboard",
 		service: "GitHub Repo",
-		imageSrc: proj8image,
-		url: "https://github.com/ystamaritq/note-taker",
+		imageSrc: proj4image,
+		url: "https://ystamaritq.github.io/weather-dashboard/",
 	},
 	{
 		id: 9,
-		title: "Employee Tracker",
+		title: "Work Day Scheduler",
 		service: "GitHub Repo",
-		imageSrc: proj9image,
-		url: "https://github.com/ystamaritq/employee-tracker",
+		imageSrc: proj2image,
+		url: "https://github.com/ystamaritq/work-day-scheduler",
 	},
 	{
 		id: 10,
-		title: "Employee Tracker",
+		title: "Password Generator",
 		service: "GitHub Repo",
-		imageSrc: proj9image,
-		url: "https://github.com/ystamaritq/employee-tracker",
-	},
-	{
-		id: 11,
-		title: "Employee Tracker",
-		service: "GitHub Repo",
-		imageSrc: proj9image,
-		url: "https://github.com/ystamaritq/employee-tracker",
+		imageSrc: proj1image,
+		url: "https://github.com/ystamaritq/password-generator",
 	},
 ];
