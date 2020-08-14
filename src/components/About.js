042db9f2508481ebb49a066309cf288505efbb20
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-14 12:18:19 -0500

    chnage about me p

--- src/components/About.js
+++ src/components/About.js
@@ -24,11 +24,12 @@ const About = () => {
 					</Fade>
 					<Fade bottom>
 						<p>
-							My name is Yadira Tamarit. I’m originally from Cuba. I’m a mom of
-							two wonderful boys.I have a Bachelor Degree in Computer Science. I
-							am currently enrolled in a UT Coding Bootcamp(The University of
-							Texas at Austin) at Full Stack Developer course in order to learn
-							all new technologies and continue growing my career.
+							I’m originally from Cuba. I’m a mom of two wonderful boys.I have a
+							Bachelor Degree in Computer Science. I am passionate about coding,
+							solving problems and UI design. In order to further expand my
+							coding expertise and learn new web technologies, I am currently
+							enrolled in the Full Stack Developer Coding Bootcamp, at The
+							University of Texas at Austin.
 						</p>
 						<p>
 							I love mathematics for that reason I choose computer science as a
@@ -38,11 +39,10 @@ const About = () => {
 							hard-worker, highly organized and able to work under pressure.
 						</p>
 						<p>
-							I love studying and I’m open to learn when is need it. I studied
-							Computer Sciences in (Universidad de las Ciencias Informaticas)
-							located in Havana, Cuba from (2003 - 2008).For more information
-							about my professional career, please reference the provided links
-							below:
+							I’m open to learn when is need it. I studied Computer Sciences in
+							(Universidad de las Ciencias Informaticas) located in Havana, Cuba
+							from (2003 - 2008).For more information about my professional
+							career, please reference the provided links below:
 						</p>
 						<div>
 							<a href="https://github.com/ystamaritq" target="_blank">
