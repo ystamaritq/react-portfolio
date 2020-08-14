14cd8efeef72280d597db9538b106002a2fa852a
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-14 15:31:33 -0500

    added app js the new section skills

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
+import Skills from "./components/Skills";
 import Work from "./components/work";
 import Contact from "./components/Contact";
 import NavigationBar from "./components/Navbar";
@@ -13,9 +14,11 @@ const App = () => {
 			<div className="App-header">
 				<Header></Header>
 			</div>
-
 			<div className="About">
 				<About></About>
+			</div>
+			<div className="Skills">
+				<Skills></Skills>
 			</div>
 			<div className="work">
 				<Work></Work>
