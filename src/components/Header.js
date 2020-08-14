e5340fb8908983717814edef9ad6eafb8f0ad4bd
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-14 11:38:00 -0500

    removed logo from nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
-import logo from "../assets/imgs/logo.png";
 import { Container } from "react-bootstrap";
 
 const Header = () => {
 	{
 		return (
 			<Container className="py-5">
-				<h2 className="heading-background">Yadira's Portfolio</h2>
+				<h1 className="heading-background">Yadira's Portfolio</h1>
 				<Fade bottom>
-					<h2 className="header-title">
+					<h2 className="header-title pb-md-5">
 						Hi, my name is Yadira Tamarit<br></br>
 						I'm a Full Stack Developer
+						<br></br>
+						<br></br>
+						<br></br>
+						<br></br>
+						<br></br>
 						<br></br>
 					</h2>
 				</Fade>
