86e5928cc2979225ef8b68761c1dd316685d4f43
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-14 23:39:58 -0500

    Auto close navbar on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
 import { Nav, Navbar, Image } from "react-bootstrap";
 import Fade from "react-reveal/Fade";
 import logo from "../assets/imgs/logo.png";
 
 const NavigationBar = () => {
+	const [navExpanded, setNavExpanded] = useState(false);
+
 	const scrollToTop = () => {
 		scroll.scrollToTop();
+		closeNav();
+	};
+
+	const closeNav = () => {
+		setNavExpanded(false);
 	};
 
 	return (
 		<Fade bottom cascade>
-			<Navbar expand="md" className="p-md-5" fixed="top">
+			<Navbar
+				onToggle={setNavExpanded}
+				expanded={navExpanded}
+				expand="md"
+				className="p-md-5"
+				fixed="top"
+			>
 				<Navbar.Brand href="/">
 					<Image src={logo} className="ml-md-5 ml-sm-2 custom-logo" />
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="navbar-nav" />
-				<Navbar.Collapse>
+				<Navbar.Collapse id="navbar-nav">
 					<Nav className="ml-auto">
 						<Link
 							className="link"
@@ -38,6 +51,7 @@ const NavigationBar = () => {
 							smooth={true}
 							offset={-70}
 							duration={500}
+							onClick={closeNav}
 						>
 							About
 						</Link>
@@ -49,6 +63,7 @@ const NavigationBar = () => {
 							smooth={true}
 							offset={-70}
 							duration={500}
+							onClick={closeNav}
 						>
 							Skills
 						</Link>
@@ -60,6 +75,7 @@ const NavigationBar = () => {
 							smooth={true}
 							offset={-70}
 							duration={500}
+							onClick={closeNav}
 						>
 							Work
 						</Link>
@@ -71,6 +87,7 @@ const NavigationBar = () => {
 							smooth={true}
 							offset={-70}
 							duration={500}
+							onClick={closeNav}
 						>
 							Contact
 						</Link>
