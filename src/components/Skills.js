f729cb60586119a332ead8692e606979e15c10bc
Yadira Tamarit <ystamaritq@gmail.com>
2020-10-10 14:47:11 -0500

    added updated skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,125 +1,156 @@
 import React from "react";
-import RadialBarChart from "@bit/recharts.recharts.radial-bar-chart";
-import RadialBar from "@bit/recharts.recharts.radial-bar";
-import Legend from "@bit/recharts.recharts.legend";
 import { Container } from "react-bootstrap";
 import Fade from "react-reveal/Fade";
-import ResponsiveContainer from "@bit/recharts.recharts.responsive-container";
 
-const data = [
-	{
-		name: "",
-		uv: 0,
-		pv: 360,
-		fill: "white",
-	},
-	{
-		name: "Html",
-		uv: 100,
-		pv: 360,
-		fill: "#f94144",
-	},
-	{
-		name: "Css",
-		uv: 100,
-		pv: 360,
-		fill: "#f3722c",
-	},
-	{
-		name: "JavaScript",
-		uv: 90,
-		pv: 360,
-		fill: "#f8961e",
-	},
-	{
-		name: "React",
-		uv: 100,
-		pv: 360,
-		fill: "#f9c74f",
-	},
-	{
-		name: "Node",
-		uv: 99,
-		pv: 360,
-		fill: "#90be6d",
-	},
-	{
-		name: "Rest APIs",
-		uv: 90,
-		pv: 360,
-		fill: "#43aa8b",
-	},
-	{
-		name: "ORM",
-		uv: 90,
-		pv: 360,
-		fill: "#577590",
-	},
-	{
-		name: "MySQL",
-		uv: 75,
-		pv: 360,
-		fill: "#a37774",
-	},
-	{
-		name: "Postgres",
-		uv: 75,
-		pv: 360,
-		fill: "#484a47",
-	},
-	{
-		name: "Mongo",
-		uv: 80,
-		pv: 360,
-		fill: "#8a817c",
-	},
-	{
-		name: "Java",
-		uv: 50,
-		pv: 360,
-		fill: "#c97c5d",
-	},
+const skillsData = [
+	{ name: "html", color: "#00a896" },
+	{ name: "css", color: "#457b9d" },
+	{ name: "react", color: "#d90429" },
+	{ name: "node", color: "#6d6875" },
+	{ name: "express", color: "#f77f00" },
+	{ name: "jquery", color: "#003049" },
+	{ name: "apis", color: "#fca311" },
+	{ name: "mongodb", color: "#14213d" },
+	{ name: "postgres", color: "#432818" },
+	{ name: "mysql", color: "#83944C" },
+	{ name: "testing", color: "#4361ee" },
+	{ name: "bootstrap", color: "#e0b1cb" },
+	{ name: "pure", color: "#3e1f47" },
+	{ name: "ui/ux", color: "#333533" },
+	{ name: "materialize", color: "#7f7f7f" },
+	{ name: "git", color: "#5e503f" },
+	{ name: "graphql", color: "#7fb800" },
+	{ name: "antd", color: "#abc4ff" },
 ];
-
-const style = {
-	lineHeight: "35px",
-	right: 0,
-};
 
 const Skills = () => {
 	return (
 		<Container className="skills">
 			<Fade bottom>
-				<h1 className="heading mb-2">
+				<h1 className="heading mb-5">
 					<Fade bottom cascade>
 						Skills
 					</Fade>
 				</h1>
 			</Fade>
 			<Fade bottom>
-				<ResponsiveContainer height={700}>
-					<RadialBarChart data={data}>
-						<Legend
-							iconSize={20}
-							layout="vertical"
-							verticalAlign="middle"
-							hizontalAlign="end"
-							wrapperStyle={style}
-						/>
-						<RadialBar
-							innerRadius={100}
-							minAngle={25}
-							label={{
-								position: "insideStart",
-								fill: "#fff",
-								fontWeight: "bolder",
+				<div className="d-flex align-content-start flex-wrap">
+					{skillsData.map((skill, index) => (
+						<span
+							key={index}
+							style={{
+								borderRadius: "12px",
+								background: skill.color,
+								padding: "12px",
+								width: "100px",
+								marginRight: "10px",
+								height: "50px",
+								marginBottom: "10px",
+								textAlign: "center",
+								color: "white",
 							}}
-							background
-							clockWise
-							dataKey="uv"
-						/>
-					</RadialBarChart>
-				</ResponsiveContainer>
+						>
+							{skill.name}
+						</span>
+					))}
+
+					{/* <span
+						style={{
+							borderRadius: "20px",
+							background: "#73AD21",
+							padding: "2px",
+							width: "100px",
+							marginRight: "10px",
+							height: "50px",
+						}}
+					>
+						Html
+					</span>
+					<span
+						style={{
+							borderRadius: "20px",
+							background: "#73AD21",
+							padding: "2px",
+							width: "100px",
+							marginRight: "10px",
+							height: "50px",
+						}}
+					>
+						Html
+					</span>
+					<span
+						style={{
+							borderRadius: "20px",
+							background: "#73AD21",
+							padding: "2px",
+							width: "100px",
+							marginRight: "10px",
+							height: "50px",
+						}}
+					>
+						Html
+					</span>
+					<span
+						style={{
+							borderRadius: "20px",
+							background: "#73AD21",
+							padding: "2px",
+							width: "100px",
+							marginRight: "10px",
+							height: "50px",
+						}}
+					>
+						Html
+					</span>
+					<span
+						style={{
+							borderRadius: "20px",
+							background: "#73AD21",
+							padding: "2px",
+							width: "100px",
+							marginRight: "10px",
+							height: "50px",
+						}}
+					>
+						Html
+					</span>
+					<span
+						style={{
+							borderRadius: "20px",
+							background: "#73AD21",
+							padding: "2px",
+							width: "100px",
+							marginRight: "10px",
+							height: "50px",
+						}}
+					>
+						Html
+					</span>
+					<span
+						style={{
+							borderRadius: "20px",
+							background: "#73AD21",
+							padding: "2px",
+							width: "100px",
+							marginRight: "10px",
+							height: "50px",
+						}}
+					>
+						Html
+					</span>
+					<span
+						style={{
+							borderRadius: "20px",
+							background: "#73AD21",
+							padding: "2px",
+							width: "100px",
+							marginRight: "10px",
+							height: "50px",
+						}}
+					>
+						Html
+					</span> */}
+				</div>
 			</Fade>
 		</Container>
 	);
